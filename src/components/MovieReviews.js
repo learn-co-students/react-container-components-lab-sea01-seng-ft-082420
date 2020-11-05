adc7f155af9876e63e7baec3b852fdc611271b7a
Cooliomoded <Cooliomoded@gmail.com>
2020-11-05 13:16:34 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ( { review } ) => {
+    
+    return(
+        <div className='review-list'>
+            <ul className='review'>
+                <li><h3>{review.display_title}</h3></li>
+                <li><img typeof={review.multimedia.type} src={review.multimedia.src} alt='broken'></img></li>
+                <li>MPAA Rating: {review.mpaa_rating}</li>
+                <li>Critics Pick: {review.critics_pick}</li>
+                <li>Opening Date: {review.opening_date ? review.opening_date : "This movie is still in production and has not set an opening date."}</li>
+                <li><h4>{review.headline}</h4></li>
+                <li>Review Published: {review.publication_date}</li>
+                <li>Critic: {review.byline}</li>
+                <li>{console.log(review)}</li>
+                <li>{review.summary_short}</li>
+                <li><a href={review.link.url}>{review.link.suggested_link_text}</a></li>
+            </ul> 
+        </div>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,29 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+        searchTerm: null
+    }
+    
+    componentDidMount() {
+        fetch(`${URL}`)
+            .then(res => res.json())
+            .then(data => this.setState({
+                reviews: data.results
+            }))
+    }
+    
+    render() {
+        const { reviews, searchTerm } = this.state
+        return(
+            <div className='searchable-movie-reviews'>
+                {reviews.map(review => <MovieReviews review={review} />)}
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
