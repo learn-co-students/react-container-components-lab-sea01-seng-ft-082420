adc6369ec33da865badeb4d023b687ff78b3ae6c
Chanson9892 <chan.hanson5@gmail.com>
2020-11-05 11:32:28 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+  
+import React from 'react'
+
+const MovieReviews = ({ reviews }) => {
+    return (
+        <ol className='review-list'>
+            {reviews.map(review => {
+                return(
+                    <li className='review'>
+                        <h3><a href={review['link']['url']}>{review['headline']}</a></h3>
+                        {review['critics_pick'] ? <strong>Critics Pick!<br /></strong>: null}
+                        <i>{review['summary_short']}</i>
+                    </li>
+                )
+            })}
+        </ol>
+    );
+};
+
+export default MovieReviews
